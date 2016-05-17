6010af3dcb0d881855b0fc4677bf1a995a6ee9c8
Kevin Lin <kevinptt0323@gmail.com>
2016-05-17 19:38:37 +0800

    position:fixed appbar

--- src/js/App.js
+++ src/js/App.js
@@ -16,7 +16,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.onLeftIconButtonTouchTap = this.onLeftIconButtonTouchTap.bind(this);
-    this.state = { token: null };
+    this.state = { token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjg5LCJpc3MiOiJodHRwOlwvXC9jc3d3d2Rldi5jcy5uY3R1LmVkdS50dzo3MTIyXC9hcGlcL2xvZ2luIiwiaWF0IjoxNDYzNDg0MzQ1LCJleHAiOjE0NjM0ODc5NDUsIm5iZiI6MTQ2MzQ4NDM0NSwianRpIjoiZThlYTJlZTI2ZmVjZGZkYmEwOWYyNGE3MDk5OTBmOTMifQ.fL7-ZNPuEcVJNmAP_HmS_6r05XygstDe2_GgECkh5XM" };
     this.setToken = this.setToken.bind(this);
     this.getToken = this.getToken.bind(this);
   }
@@ -35,12 +35,15 @@ class App extends React.Component {
         <div style={{width: '100vw', height: '100vh'}}>
           <AppBar
             onLeftIconButtonTouchTap={this.onLeftIconButtonTouchTap}
+            style={{ position: 'fixed' }}
             title="Hello, world!"
           />
           <LeftNav ref="leftNav" />
           <div
             style={{
+              position: 'relative',
               height: 'calc(100% - 64px)',
+              top: '64px'
             }}
           >
             {
