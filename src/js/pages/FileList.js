4bf8556cd2c5f0d8a3720a601e7b8c2f31d474bf
Kevin Lin <kevinptt0323@gmail.com>
2016-05-17 19:32:35 +0800

    add filelist page

--- src/js/pages/FileList.js
+++ src/js/pages/FileList.js
@@ -1,42 +1,56 @@
 import React from 'react';
 import { Paper } from 'material-ui';
 import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+import request from 'superagent';
+import superagent_prefix from 'superagent-prefix';
+
+const prefix = superagent_prefix('http://cswwwdev.cs.nctu.edu.tw:7122');
 
 class FileList extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = { lists: [] };
+    this.loadData();
+  }
+  loadData() {
+    request.get('/api/data')
+      .use(prefix)
+      .set('Authorization', `Bearer ${this.props.getToken()}`)
+      .accept('json')
+      .end((err, res) => {
+        if( err ) {
+          console.error(err);
+        } else {
+          this.setState({ lists: res.body.data });
+        }
+      });
+
   }
   render() {
+    console.log(this.state.lists);
+    let table_rows = this.state.lists.map(file => (
+      <TableRow key={file.id}>
+        <TableRowColumn>{file.id}</TableRowColumn>
+        <TableRowColumn>{file.filename}</TableRowColumn>
+        <TableRowColumn>{JSON.stringify(file.metadata)}</TableRowColumn>
+        <TableRowColumn>{file.created_at}</TableRowColumn>
+        <TableRowColumn>{file.updated_at}</TableRowColumn>
+      </TableRow>
+    ));
     return (
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHeaderColumn>ID</TableHeaderColumn>
-            <TableHeaderColumn>Name</TableHeaderColumn>
-            <TableHeaderColumn>Status</TableHeaderColumn>
+            <TableHeaderColumn>id</TableHeaderColumn>
+            <TableHeaderColumn>filename</TableHeaderColumn>
+            <TableHeaderColumn>metadata</TableHeaderColumn>
+            <TableHeaderColumn>created_at</TableHeaderColumn>
+            <TableHeaderColumn>updated_at</TableHeaderColumn>
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableRowColumn>1</TableRowColumn>
-            <TableRowColumn>John Smith</TableRowColumn>
-            <TableRowColumn>Employed</TableRowColumn>
-          </TableRow>
-          <TableRow>
-            <TableRowColumn>2</TableRowColumn>
-            <TableRowColumn>Randal White</TableRowColumn>
-            <TableRowColumn>Unemployed</TableRowColumn>
-          </TableRow>
-          <TableRow>
-            <TableRowColumn>3</TableRowColumn>
-            <TableRowColumn>Stephanie Sanders</TableRowColumn>
-            <TableRowColumn>Employed</TableRowColumn>
-          </TableRow>
-          <TableRow>
-            <TableRowColumn>4</TableRowColumn>
-            <TableRowColumn>Steve Brown</TableRowColumn>
-            <TableRowColumn>Employed</TableRowColumn>
-          </TableRow>
+          { table_rows }
         </TableBody>
       </Table>
     );
