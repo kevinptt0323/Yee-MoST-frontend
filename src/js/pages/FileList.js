3c4a344d71cff74d6a0f0065d6cdc24455d7ebd6
Kevin Lin <kevinptt0323@gmail.com>
2016-05-19 14:02:36 +0800

    display 10 rows per page in FileList

--- src/js/pages/FileList.js
+++ src/js/pages/FileList.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router'
 import { Paper, IconButton } from 'material-ui';
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn, TableFooter} from 'material-ui/Table';
+
 import FileDownload from 'material-ui/svg-icons/file/file-download';
 import Search from 'material-ui/svg-icons/action/search';
-import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+import NavLeft from 'material-ui/svg-icons/navigation/chevron-left';
+import NavRight from 'material-ui/svg-icons/navigation/chevron-right';
+
 import request from 'superagent';
 import superagent_prefix from 'superagent-prefix';
 
 const prefix = superagent_prefix('http://cswwwdev.cs.nctu.edu.tw:7122');
+
+const ROWS_PER_PAGE = 10;
 
 class FileList extends React.Component {
   constructor(props) {
@@ -31,7 +37,9 @@ class FileList extends React.Component {
 
   }
   render() {
-    let table_rows = this.state.lists.map(file => (
+    let { page = 1 } = this.props.location.query;
+    page = parseInt(page);
+    const table_rows = this.state.lists.slice((page-1)*ROWS_PER_PAGE, page*ROWS_PER_PAGE).map(file => (
       <TableRow key={file.id}>
         <TableRowColumn>{file.id}</TableRowColumn>
         <TableRowColumn>{file.filename}</TableRowColumn>
@@ -42,6 +50,18 @@ class FileList extends React.Component {
         <TableRowColumn>{<IconButton><FileDownload /></IconButton>}</TableRowColumn>
       </TableRow>
     ));
+    const navLeft =
+      page>1 ? (
+        <Link to={{ pathname: '/files', query: {page: page-1} }}><IconButton><NavLeft /></IconButton></Link>
+      ) : (
+        <IconButton disabled={true}><NavLeft /></IconButton>
+      );
+    const navRight =
+      page*ROWS_PER_PAGE<this.state.lists.length ? (
+        <Link to={{ pathname: '/files', query: {page: page+1} }}><IconButton><NavRight /></IconButton></Link>
+      ) : (
+        <IconButton disabled={true}><NavRight /></IconButton>
+      );
     return (
       <Table>
         <TableHeader>
@@ -58,6 +78,14 @@ class FileList extends React.Component {
         <TableBody preScanRows={false}>
           { table_rows }
         </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TableRowColumn colSpan="7" style={{textAlign: 'right'}}>
+              { navLeft }
+              { navRight }
+            </TableRowColumn>
+          </TableRow>
+        </TableFooter>
       </Table>
     );
   }
